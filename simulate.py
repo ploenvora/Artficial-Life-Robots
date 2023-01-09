a1785907e4eb79d532afc20e58ae46d9d1538368
ploenvora <p.voraprukpisut@gmail.com>
2023-01-09 14:15:35 -0600

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -1,4 +1,13 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+
+#insert for-loop that iterates 1000 times
+for i in range(1000):
+    p.stepSimulation()
+    print("Start : %s" % time.ctime())
+    time.sleep(5)
+    print("End : %s" % time.ctime())
+
 p.disconnect()
