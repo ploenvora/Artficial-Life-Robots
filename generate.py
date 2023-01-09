b03329bfc329be7be1fcac10d91ebcba764d3781
ploenvora <p.voraprukpisut@gmail.com>
2023-01-09 17:42:01 -0600

    lets commit because we are done :)

--- generate.py
+++ generate.py
@@ -1,9 +1,5 @@
 import pyrosim.pyrosim as pyrosim
 
-#Tells pyrosim the name of the file where information about the world you're about to create 
-# should be stored. This world will currently be called box, because it will only contain a box.
-pyrosim.Start_SDF("boxes.sdf")
-
 #Stores a box with initial position x=0, y=0, z=0.5, and height, length and width all
 # equal to 1 meter, in box.sdf.
 height = 1
@@ -15,22 +11,26 @@ z = 0.5
 x2 = 1
 y2 = 0
 z2 = 1.5
-#pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[width, length, height])
-#pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2] , size=[width, length, height])
 
-x_change = 0
+def Create_World():
+    #Tells pyrosim the name of the file where information about the world you're about to create 
+    # should be stored. This world will currently be called box, because it will only contain a box.
+    pyrosim.Start_SDF("world.sdf")
+    
+    pyrosim.Send_Cube(name="Box", pos=[x + 10,y + 10,z] , size=[width, length, height])
+    #pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2] , size=[width, length, height])
 
-for i in range(5):
-    y_change = 0
-    for i in range(5):
-        z_change = 0.5
-        size_change = 1
-        for i in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[x_change,y_change,z_change] , size=[size_change, size_change, size_change])
-            z_change = z_change + 1
-            size_change = size_change * 0.9
-        y_change = y_change + 1
-    x_change = x_change + 1
+    #Tells pyrosim to close the sdf file.
+    pyrosim.End()
 
-#Tells pyrosim to close the sdf file.
-pyrosim.End()
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] , size=[width, length, height])
+    pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5] , size=[width, length, height])
+    pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [2,0,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5] , size=[width, length, height])
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -11,14 +11,17 @@ p.setGravity(0,0,-9.8)
 #Adding a floor
 planeId = p.loadURDF("plane.urdf")
 
+#Adding a robot
+robotId = p.loadURDF("body.urdf")
+
 #Tells pybullet to read in the world described in box.sdf
-p.loadSDF("boxes.sdf")
+p.loadSDF("world.sdf")
 
 #insert for-loop that iterates 1000 times
 for i in range(1000):
     p.stepSimulation()
-    print("Start : %s" % time.ctime())
+   #print("Start : %s" % time.ctime())
     time.sleep(1/60)
-    print("End : %s" % time.ctime())
+    #print("End : %s" % time.ctime())
 
 p.disconnect()
